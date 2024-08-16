9e00e252e542504993fcb6af17a6e2a71a21972c
baabguui <9universe9th@gmail.com>
2024-08-16 22:04:22 +0900

    ✨ Add Todo model
    swift data

--- maa/Model/Todo.swift
+++ maa/Model/Todo.swift
@@ -5,4 +5,56 @@
 //  Created by baabguui on 8/16/24.
 //
 
-class Todo {}
+import Foundation
+import SwiftData
+
+@Model
+class Todo {
+    let id = UUID()
+    var categoryRawValue: String
+    var category:Category {
+        get {
+            Category(rawValue: categoryRawValue) ?? .day
+        }
+        set {
+            categoryRawValue = newValue.rawValue
+        }
+    }
+    var index: Int
+    var content: String
+    var checkDate: Date
+    var isDone: Bool {
+        switch self.category {
+        case .day:
+            return Calendar.current.isDate(
+                self.checkDate,
+                equalTo: Date(),
+                toGranularity: .day)
+        case .week:
+            return Calendar.current.isDate(
+                self.checkDate,
+                equalTo: Date(),
+                toGranularity: .weekOfYear)
+        case .event:
+            return false // TODO: - checkDate가 저번주 목요일 0시 ~ 이번주 목요일 0시 사이인지 확인
+        }
+    }
+    // MARK: - 삭제 가능 여부
+    var isCustom: Bool
+    
+    init(categoryRawValue: String, index: Int, content: String, checkDate: Date, isCustom: Bool) {
+        self.categoryRawValue = categoryRawValue
+        self.index = index
+        self.content = content
+        self.checkDate = checkDate
+        self.isCustom = isCustom
+    }
+    
+    
+}
+
+enum Category: String {
+    case day
+    case week
+    case event
+}
