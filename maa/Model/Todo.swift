4b2571e0273263174f88f13b5c7efdba1468d6fc
baabguui <9universe9th@gmail.com>
2024-08-17 01:58:11 +0900

    ♻️ Todo model checkDate optional 처리
    Category 프로퍼티 추가

--- maa/Model/Todo.swift
+++ maa/Model/Todo.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftData
+import SwiftUI
 
 @Model
 class Todo {
@@ -22,17 +23,18 @@ class Todo {
     }
     var index: Int
     var content: String
-    var checkDate: Date
+    var checkDate: Date?
     var isDone: Bool {
+        guard let check = checkDate else { return false }
         switch self.category {
         case .day:
             return Calendar.current.isDate(
-                self.checkDate,
+                check,
                 equalTo: Date(),
                 toGranularity: .day)
         case .week:
             return Calendar.current.isDate(
-                self.checkDate,
+                check,
                 equalTo: Date(),
                 toGranularity: .weekOfYear)
         case .event:
@@ -42,19 +44,43 @@ class Todo {
     // MARK: - 삭제 가능 여부
     var isCustom: Bool
     
-    init(categoryRawValue: String, index: Int, content: String, checkDate: Date, isCustom: Bool) {
+    init(categoryRawValue: String, index: Int, content: String, checkDate: Date? = nil, isCustom: Bool) {
         self.categoryRawValue = categoryRawValue
         self.index = index
         self.content = content
         self.checkDate = checkDate
         self.isCustom = isCustom
     }
-    
-    
 }
 
-enum Category: String {
+enum Category: String, CaseIterable {
     case day
     case week
     case event
+    
+    var deadline: String {
+        switch self {
+        case .day:
+            return "매일 0시까지"
+        case .week:
+            return "월요일 0시까지"
+        case .event:
+            return "목요일 0시까지"
+        }
+    }
+    
+    var categoryColor: Color {
+        switch self {
+        case .day:
+            return Color.first
+        case .week:
+            return Color.second
+        case .event:
+            return Color.third
+        }
+    }
+    
+    static var allCase: [Category] {
+        Category.allCases.map {$0}
+    }
 }
