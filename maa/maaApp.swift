54883f41fa89b06d091073b31d86971f54ea5568
baabguui <9universe9th@gmail.com>
2024-08-16 21:19:56 +0900

    📦️ Add firebase
    background mode, push notification (capability)

--- maa/maaApp.swift
+++ maa/maaApp.swift
@@ -6,12 +6,29 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
-struct maaApp: App {
-    var body: some Scene {
-        WindowGroup {
-            TodoView()
-        }
+struct YourApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentContainer()
+      }
     }
+  }
 }
+
