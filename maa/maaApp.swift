1b02420b195bcf367f5c2ea42fcdf8bd072fd17f
baabguui <9universe9th@gmail.com>
2024-08-16 22:38:00 +0900

    ✨ Swift data modelContainer 생성

--- maa/maaApp.swift
+++ maa/maaApp.swift
@@ -5,30 +5,39 @@
 //  Created by baabguui on 8/16/24.
 //
 
-import SwiftUI
 import FirebaseCore
+import SwiftData
+import SwiftUI
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
 }
 
 @main
 struct YourApp: App {
-  // register app delegate for Firebase setup
-  @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-
-
-  var body: some Scene {
-    WindowGroup {
-      NavigationView {
-        ContentContainer()
-      }
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    var modelContainer: ModelContainer = {
+        let schema = Schema([Todo.self])
+        let modelConfiguration = ModelConfiguration(isStoredInMemoryOnly: false)
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentContainer()
+                    .modelContainer(modelContainer)
+            }
+        }
     }
-  }
 }
 
